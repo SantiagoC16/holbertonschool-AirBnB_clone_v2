9d242418315ce613d07154b16427277f00a6c52f
Santiago Caritat <6317@holbertonstudents.com>
2023-08-02 05:35:13 -0700

    basemodel updated

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,18 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
+    id = Column(String(60), primary_key=True,
+                nullable=False, default=(str(uuid.uuid4())))
+    created_at = Column(DateTime, nullable=False, default=func.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=func.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             kwargs['updated_at'] = datetime.strptime(
                 kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
