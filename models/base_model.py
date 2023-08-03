45b251faeaf230ac572b482ab5b10abeeb95850b
Santiago Caritat <6317@holbertonstudents.com>
2023-08-03 06:42:35 -0700

    base model replace id and created at

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,9 @@ class BaseModel:
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
         else:
