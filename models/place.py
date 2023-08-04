0a94931666a81c143a0592b78aeb9af9f7236751
Santiago Caritat <6317@holbertonstudents.com>
2023-08-04 15:35:13 -0700

    task 9

--- models/place.py
+++ models/place.py
@@ -2,6 +2,8 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
@@ -18,3 +20,18 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship(
+            "Review", backref="place", cascade="all, delete, delete-orphan")
+
+    else:
+        @property
+        def reviews(self):
+            from models import storage
+            from models.review import Review
+            reviews = []
+            for review in storage.all(Review).values():
+                if review.place_id == self.id:
+                    reviews.append(review)
+            return reviews
