f89cd4b8fb741f04a9fde7f27d8ac43e38bef6c1
SantiagoC16 <6317@holbertonstudents.com>
2023-08-03 17:16:35 -0700

    task 6 almost done...

---
+++ models/engine/db_storage.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""db storage"""
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import BaseModel, Base
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
+from os import getenv
+
+models = [State, City, Amenity, Place, Review, User]
+
+
+class DBStorage():
+    """db storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """init class where we create the engine"""
+        user = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_USER")
+        host = getenv("HBNB_MYSQL_HOST")
+        database = getenv("HBNB_MYSQL_DB")
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            user, password, host, database), pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop(self.__engine)
+
+    def all(self, cls=None):
+        """all"""
+        objects = {}
+        if cls is not None:
+            if cls in models:
+                _query = self.__session.query(cls).all()
+                for obj in _query:
+                    key = "{}.{}".format(obj.__name__, obj.id)
+                    objects[key] = obj
+                return objects
+        else:
+            for cls in models:
+                _query = self.__session.query(cls).all()
+                for obj in _query:
+                    key = "{}.{}".format(obj.__name__, obj.id)
+                    objects[key] = obj
+
+    def new(self, obj):
+        """comment"""
+        pass
+
+    def save(self):
+        """comment"""
+        pass
+
+    def delete(self, obj=None):
+        """comment"""
+        pass
+
+    def reload(self):
+        """comment"""
+        pass
--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from models.city import City
 from os import getenv
 
 Base = declarative_base()
@@ -14,9 +15,17 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    place = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """setter cities"""
-        cities = []
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place = relationship(
+            "City", backref="state", cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """getter cities"""
+            from models import storage
+            cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
