eaf09c126008f57a9b2e34448f7c97f29f37c8a1
Santiago Caritat <6317@holbertonstudents.com>
2023-08-15 08:57:34 -0700

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def num(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_template(n):
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', numb=n)
 
 
 if __name__ == '__main__':
