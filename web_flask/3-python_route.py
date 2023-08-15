054ae0418a1e13dc44b3244f21bc632cfcdc9163
Santiago Caritat <6317@holbertonstudents.com>
2023-08-15 07:03:30 -0700

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,7 @@ def c_text(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 def python_text(text="is cool"):
     text = text.replace('_', ' ')
     return "python {}".format(text)
