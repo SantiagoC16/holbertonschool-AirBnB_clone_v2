c6abdaf0f9635f86ae7302394ba9ce20dd05b849
Santiago Caritat <6317@holbertonstudents.com>
2023-08-15 11:49:48 -0700

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    from models import storage
+    from models.state import State
     states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda state: state.name)
-
     return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
+    """closes the sql session"""
+    from models import storage
     storage.close()
 
 
