b2e85b57481389f269f92c6e7288e49549f0848d
Santiago Caritat <6317@holbertonstudents.com>
2023-08-15 07:05:39 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     text = text.replace('_', ' ')
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 if __name__ == '__main__':
