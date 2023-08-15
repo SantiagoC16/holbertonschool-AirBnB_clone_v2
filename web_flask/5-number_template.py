ff2170184d61c0156cebb1b653dc7c1907c52f9f
Santiago Caritat <6317@holbertonstudents.com>
2023-08-15 08:54:41 -0700

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,8 +33,8 @@ def num(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def num_template(n):
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
     return render_template("5-number.html", n=n)
 
 
