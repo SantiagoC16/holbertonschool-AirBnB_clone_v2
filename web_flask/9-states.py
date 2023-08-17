363ed632bae5ea2b770de06fa5171c536c92242e
Santiago Caritat <6317@holbertonstudents.com>
2023-08-17 10:13:41 -0700

    task 9 started

---
+++ web_flask/9-states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def cities_by_states():
+    from models import storage
+    from models.state import State
+    states = storage.all(State)
+
+    return render_template('9-states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """closes the sql session"""
+    from models import storage
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
